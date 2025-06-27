821db0f320eacb1e7ec46b6b69ed8e9ab04036f5
Kyung Lee <kyung.lee.official@outlook.com>
2025-06-27 11:51:51 +0800

    style: update image layout and improve responsiveness in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,13 +39,21 @@ export default function Home() {
 					堅持自然光拍攝，因為我知道：真實的顏色，才能經得起你目光的考驗。
 				</p>
 				<div className="flex justify-center w-full">
-					<div className="max-w-[300px]">
-						<img src="images/3.webp" alt="" />
+					<div className="lg:w-[557px]">
+						<img
+							src="images/3.webp"
+							alt=""
+							className="w-full h-auto"
+						/>
 					</div>
 				</div>
 				<div className="flex justify-center w-full">
-					<div className="max-w-[440px]">
-						<img src="images/4.webp" alt="" />
+					<div className="lg:max-w-[1160px] max-w-[440px]">
+						<img
+							src="images/4.webp"
+							alt=""
+							className="w-full h-auto"
+						/>
 					</div>
 				</div>
 				<div
@@ -53,27 +61,29 @@ export default function Home() {
 					text-white"
 				>
 					<div
-						className="flex flex-col items-center w-[440px] p-6 gap-3
+						className="flex flex-col lg:flex-row items-center lg:items-start w-[440px] lg:w-[1160px] p-6 gap-3 lg:gap-6
 						bg-green-600"
 					>
 						<div className="max-w-[360px]">
 							<img src="images/5.webp" alt="" />
 						</div>
-						<p
-							className="flex justify-center w-full
-							text-3xl font-bold"
-						>
-							翡翠是買原石加工好？還是直接買成品好？
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							以我自己的角度來看，其實買翡翠原石自行加工，比直接買成品更有保障、性價比也高很多。
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							現在的翡翠市場有不少風氣：喜歡吹捧、過度包裝、靠媒體炒作、網路平台帶貨、甚至連協會頒獎都能搞得亂七八糟，只要花點錢什麼「噱頭」都能買到。
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							但這些成本，最後都會轉嫁到消費者身上——所謂的「羊毛出在羊身上」，真的是永遠不變的道理。
-						</p>
+						<div>
+							<p
+								className="flex justify-center w-full
+								text-3xl font-bold"
+							>
+								翡翠是買原石加工好？還是直接買成品好？
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								以我自己的角度來看，其實買翡翠原石自行加工，比直接買成品更有保障、性價比也高很多。
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								現在的翡翠市場有不少風氣：喜歡吹捧、過度包裝、靠媒體炒作、網路平台帶貨、甚至連協會頒獎都能搞得亂七八糟，只要花點錢什麼「噱頭」都能買到。
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								但這些成本，最後都會轉嫁到消費者身上——所謂的「羊毛出在羊身上」，真的是永遠不變的道理。
+							</p>
+						</div>
 					</div>
 				</div>
 				<div
@@ -81,30 +91,32 @@ export default function Home() {
 					text-white"
 				>
 					<div
-						className="flex flex-col items-center w-[440px] p-6 gap-3
+						className="flex flex-col lg:flex-row-reverse items-center lg:items-start w-[440px] lg:w-[1160px] p-6 gap-3 lg:gap-6
 						bg-green-600"
 					>
 						<div className="max-w-[360px]">
 							<img src="images/6.webp" alt="" />
 						</div>
-						<p
-							className="flex justify-center w-full
-							text-3xl font-bold"
-						>
-							為什麼建議你買翡翠原石
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							所有成品都是原石加工來的，自己買原石可省下大量中間商利潤。
-							原石本身有收藏與加工潛力，可做掛件、手鐲等，價值彈性大、升值空間高。
-							成品市場亂象多、不良商家多，原石反而更能確保來源與品質。
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							隨著近年翡翠走熱，市面上出現不少以次充好、以假充真的翡翠。許多商家拿最差的玉石冒充天然翡翠，價格一路炒高，飆升到幾十萬甚至幾百萬！把翡翠市場搞得烏煙瘴氣，想買天然翡翠，即使出高價也未必買到。
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							所以說買翡翠從源頭買才是最好的。
-							在大家炒作價格的時候，我們不能說價格是最低，但因為有工廠，我們絕對是性價比最高的
-						</p>
+						<div>
+							<p
+								className="flex justify-center w-full
+								text-3xl font-bold"
+							>
+								為什麼建議你買翡翠原石
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								所有成品都是原石加工來的，自己買原石可省下大量中間商利潤。
+								原石本身有收藏與加工潛力，可做掛件、手鐲等，價值彈性大、升值空間高。
+								成品市場亂象多、不良商家多，原石反而更能確保來源與品質。
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								隨著近年翡翠走熱，市面上出現不少以次充好、以假充真的翡翠。許多商家拿最差的玉石冒充天然翡翠，價格一路炒高，飆升到幾十萬甚至幾百萬！把翡翠市場搞得烏煙瘴氣，想買天然翡翠，即使出高價也未必買到。
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								所以說買翡翠從源頭買才是最好的。
+								在大家炒作價格的時候，我們不能說價格是最低，但因為有工廠，我們絕對是性價比最高的
+							</p>
+						</div>
 					</div>
 				</div>
 				<div
@@ -112,27 +124,28 @@ export default function Home() {
 					text-white"
 				>
 					<div
-						className="flex flex-col items-center w-[440px] p-6 gap-3
+						className="flex flex-col lg:flex-row items-center lg:items-start w-[440px] lg:w-[1160px] p-6 gap-3 lg:gap-6
 						bg-green-600"
 					>
 						<div className="max-w-[360px]">
 							<img src="images/7.webp" alt="" />
 						</div>
-						<p
-							className="flex justify-center w-full
-							text-3xl font-bold"
-						>
-							原石選料開料加工一條龍
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							父親和舅舅為了尋找優質原料，經常往返於曼德勒、仰光等地。拿到原料後，我負責設計與加工。我深刻體會到，想要做出好作品，就必須勤奮努力——為了找到好原料四處奔波，為了呈現完美成品，經常需要繪製多張設計稿反覆比較。
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							無論客戶從哪裡、透過何種管道購買商品，最終買家與我之間其實是基於相同的品味和預算。商家考慮的是市場行情，而買家在意的是是否真心喜愛以及是否願意信任我。多年來，我所累積的客戶幾乎都是如此——我們品味相投，他們也給予我充分的信任。
-						</p>
+						<div>
+							<p
+								className="flex justify-center w-full
+								text-3xl font-bold"
+							>
+								原石選料開料加工一條龍
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								父親和舅舅為了尋找優質原料，經常往返於曼德勒、仰光等地。拿到原料後，我負責設計與加工。我深刻體會到，想要做出好作品，就必須勤奮努力——為了找到好原料四處奔波，為了呈現完美成品，經常需要繪製多張設計稿反覆比較。
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								無論客戶從哪裡、透過何種管道購買商品，最終買家與我之間其實是基於相同的品味和預算。商家考慮的是市場行情，而買家在意的是是否真心喜愛以及是否願意信任我。多年來，我所累積的客戶幾乎都是如此——我們品味相投，他們也給予我充分的信任。
+							</p>
+						</div>
 					</div>
 				</div>
-
 				<div className="flex justify-center w-full">
 					<div className="flex flex-col items-center w-[440px] p-6 gap-3">
 						<p className="flex justify-center w-full text-lg">
@@ -154,54 +167,59 @@ export default function Home() {
 				</div>
 
 				<div className="flex justify-center w-full">
-					<div className="flex flex-col items-start w-[440px] gap-3">
-						<p>ID:雨嫣 2368</p>
-						<img src="images/8.webp" alt="" />
-						<p>ID:淑婷 wzss198</p>
-						<img src="images/9.webp" alt="" />
-					</div>
-				</div>
-
-				<div
-					className="flex justify-center w-full
-					text-white"
-				>
-					<div
-						className="flex flex-col items-center w-[440px] p-6 gap-3
-						bg-green-600"
-					>
-						<div className="max-w-[300px]">
-							<img src="images/10.webp" alt="" />
+					<div className="flex flex-col lg:flex-row items-start w-[440px] gap-3">
+						<div>
+							<p>ID:雨嫣 2368</p>
+							<img src="images/8.webp" alt="" />
 						</div>
-						<p
-							className="flex justify-center w-full
-							text-3xl font-bold"
-						>
-							翡翠是一門講究眼緣的生意
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							翡翠這行沒有固定的標準，賣出去就是錢，賣不出去就只能當寶貝收藏！翡翠的交易模式讓每個小販都有庫存壓力，眼光好的存貨少，眼光差的可能好幾年都賣不掉。我們家做了幾十年的翡翠生意，父親從一個小攤販慢慢累積客源，為了節省開支，總是穿最便宜的衣服，騎著電動車風吹日曬，每天收入只有一兩百元。我跟著父母打拼，累了就直接趴在櫃檯上睡覺。直到現在，還有很多珠寶商過著這樣的生活。
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							父親憑著誠懇踏實的性格，幾十年來累積了許多忠實客戶。如今，我們的生活品質已經改善，不必再每天凌晨四點起床擺攤。無論是翡翠原石，還是吊墜、手鐲、戒指、項鍊、擺件等成品，我們都有經營
-						</p>
+						<div>
+							<p>ID:淑婷 wzss198</p>
+							<img src="images/9.webp" alt="" />
+						</div>
 					</div>
 				</div>
 
-				<div className="flex justify-center w-full">
+				<div className="flex flex-col lg:grid lg:grid-cols-2 justify-center items-start lg:items-stretch lg:w-[1160px] max-w-[440px] lg:max-w-[1160px] mx-auto gap-3">
 					<div
-						className="flex flex-col items-center w-[440px] p-6 gap-3
+						className="flex justify-center w-full h-full
+						text-white"
+					>
+						<div
+							className="flex flex-col items-center w-[440px] lg:w-full h-full p-6 gap-3
+							bg-green-600"
+						>
+							<div className="max-w-[300px]">
+								<img src="images/10.webp" alt="" />
+							</div>
+							<p
+								className="flex justify-center w-full
+							text-3xl font-bold"
+							>
+								翡翠是一門講究眼緣的生意
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								翡翠這行沒有固定的標準，賣出去就是錢，賣不出去就只能當寶貝收藏！翡翠的交易模式讓每個小販都有庫存壓力，眼光好的存貨少，眼光差的可能好幾年都賣不掉。我們家做了幾十年的翡翠生意，父親從一個小攤販慢慢累積客源，為了節省開支，總是穿最便宜的衣服，騎著電動車風吹日曬，每天收入只有一兩百元。我跟著父母打拼，累了就直接趴在櫃檯上睡覺。直到現在，還有很多珠寶商過著這樣的生活。
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								父親憑著誠懇踏實的性格，幾十年來累積了許多忠實客戶。如今，我們的生活品質已經改善，不必再每天凌晨四點起床擺攤。無論是翡翠原石，還是吊墜、手鐲、戒指、項鍊、擺件等成品，我們都有經營
+							</p>
+						</div>
+					</div>
+					<div
+						className="flex justify-center w-full 
 						border-1 border-neutral-500"
 					>
-						<div className="max-w-[360px]">
-							<img src="images/11.webp" alt="" />
+						<div className="flex flex-col items-center w-[440px] lg:w-full p-6 gap-3">
+							<div className="max-w-[360px]">
+								<img src="images/11.webp" alt="" />
+							</div>
+							<p className="flex justify-center w-full text-lg">
+								翡翠原料經過加工成為成品後，會根據雕工和品相分為不同等級。這些成品在行家之間流通交易，可能轉手一兩次，也可能轉手四五次，參與交易的有中國商人，也有緬甸商人。最後才會進入零售櫃檯，或是出現在各大網路平台上銷售。
+							</p>
+							<p className="flex justify-center w-full text-lg">
+								看到這裡，可能有人會驚呼：那我之前買的翡翠豈不是貴了很多！多不划算啊！但請想想，你喝的牛奶難道是直接從乳牛到嘴裡的嗎？從養殖開始，到規模化生產，再到廣告投放和品牌經營，中間經過多少環節。市場上看到的價格，其實都是經過這些環節博弈後產生的合理價格。
+							</p>
 						</div>
-						<p className="flex justify-center w-full text-lg">
-							翡翠原料經過加工成為成品後，會根據雕工和品相分為不同等級。這些成品在行家之間流通交易，可能轉手一兩次，也可能轉手四五次，參與交易的有中國商人，也有緬甸商人。最後才會進入零售櫃檯，或是出現在各大網路平台上銷售。
-						</p>
-						<p className="flex justify-center w-full text-lg">
-							看到這裡，可能有人會驚呼：那我之前買的翡翠豈不是貴了很多！多不划算啊！但請想想，你喝的牛奶難道是直接從乳牛到嘴裡的嗎？從養殖開始，到規模化生產，再到廣告投放和品牌經營，中間經過多少環節。市場上看到的價格，其實都是經過這些環節博弈後產生的合理價格。
-						</p>
 					</div>
 				</div>
 
@@ -213,7 +231,7 @@ export default function Home() {
 				</p>
 				<div className="flex justify-center w-full">
 					<div
-						className="flex flex-col items-center w-[440px] p-6 gap-3
+						className="flex flex-col items-center w-[440px] lg:w-[1160px] p-6 gap-3
 						bg-sky-200"
 					>
 						<div className="max-w-[360px]">
@@ -235,7 +253,7 @@ export default function Home() {
 				</div>
 
 				<div className="flex justify-center w-full">
-					<div className="flex flex-col items-center w-[440px] gap-3">
+					<div className="flex flex-col items-center w-[440px] lg:w-[1160px] gap-3">
 						<img src="images/13.webp" alt="" />
 					</div>
 				</div>
@@ -251,30 +269,49 @@ export default function Home() {
 					>
 						如何聯繫翡翠專業顧問？
 					</p>
-					<div className="w-[440px]">
-						<p className="flex justify-center w-full text-lg">
+					<div className=" w-[440px] lg:w-[1160px]">
+						<p
+							className="flex justify-center w-full py-3
+							text-lg"
+						>
 							在翡翠產業深耕多年，累積了豐富的專業知識與經驗。我們始終秉持誠信經營的理念，用心為每位顧客服務。每天清晨，當城市還在沉睡時，我們已開始為尋找優質翡翠而努力工作。
 						</p>
-						<p className="flex justify-center w-full text-lg">
+						<p
+							className="flex justify-center w-full py-3
+							text-lg"
+						>
 							我們的翡翠原料直接來自緬甸礦區，經過嚴格挑選和加工，確保每一件產品都符合高品質標準。無論是原石、吊墜、手鐲、戒指還是藝術擺件，我們都能提供專業的建議和服務。我們直接從礦區取得第一手翡翠原料，由專業團隊進行設計、加工，所有工序都在自有工坊完成。每天都會在LINE動態分享最新的翡翠原石、首飾及藝術擺件等商品資訊，歡迎追蹤欣賞。
 						</p>
-						<p className="flex justify-center w-full text-lg">
+						<p
+							className="flex justify-center w-full py-3
+							text-lg"
+						>
 							許多顧客告訴我們，光是瀏覽我們分享的翡翠資訊就是一種享受。不購買也沒關係，歡迎透過LINE交流翡翠知識。無論您對翡翠原石、吊墜、手鐲、戒指、項鍊或擺件有任何疑問，我們都很樂意為您解答。
 						</p>
-						<p className="flex justify-center w-full text-lg">
+						<p
+							className="flex justify-center w-full py-3
+							text-lg"
+						>
 							想了解更多翡翠資訊或鑑賞專業建議，歡迎加入我們的LINE官方帳號：
 						</p>
-						<p className="flex justify-center w-full text-lg">
+						<p
+							className="flex justify-center w-full py-3
+							text-lg"
+						>
 							📲 LINE 加好友： 【雨嫣 2368】 | 【淑婷 wzss198】
 						</p>
 					</div>
 				</div>
 				<div className="flex justify-center w-full">
-					<div className="flex flex-col items-start w-[440px] gap-3">
-						<p>ID:雨嫣 2368</p>
-						<img src="images/8.webp" alt="" />
-						<p>ID:淑婷 wzss198</p>
-						<img src="images/9.webp" alt="" />
+					<div className="flex flex-col lg:flex-row items-start w-[440px] gap-3">
+						<div>
+							<p>ID:雨嫣 2368</p>
+							<img src="images/8.webp" alt="" />
+						</div>
+						<div>
+							<p>ID:淑婷 wzss198</p>
+							<img src="images/9.webp" alt="" />
+						</div>
 					</div>
 				</div>
 			</main>
